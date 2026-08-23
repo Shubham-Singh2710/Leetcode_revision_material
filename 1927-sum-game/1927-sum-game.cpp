7be23bbea55b5892d1d26bff88265df34975f075
Shubham-Singh2710 <shubhamsingh270218@gmail.com>
2026-08-23 13:12:58 +0530

    Time: 7 ms (21.45%), Space: 14.1 MB (33.57%) - LeetHub

--- 1927-sum-game/1927-sum-game.cpp
+++ 1927-sum-game/1927-sum-game.cpp
@@ -1,20 +1,28 @@
 class Solution {
- public:
-  bool sumGame(string num) {
-    const int n = num.length();
-    double ans = 0.0;
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+        int leftSum = 0, rightSum = 0;
+        int leftQ = 0, rightQ = 0;
 
-    for (int i = 0; i < n / 2; ++i)
-      ans += getExpectation(num[i]);
+        for (int i = 0; i < n / 2; i++) {
+            if (num[i] == '?'){
+                 leftQ++;
+            }
+            else leftSum += num[i] - '0';
+        }
 
-    for (int i = n / 2; i < n; ++i)
-      ans -= getExpectation(num[i]);
+        for (int i = n / 2; i < n; i++) {
+            if (num[i] == '?'){
+                 rightQ++;
+            }
+            else rightSum += num[i] - '0';
+        }
 
-    return ans != 0.0;
-  }
+     
+        if ((leftQ + rightQ) % 2 != 0) return true;
 
- private:
-  double getExpectation(char c) {
-    return c == '?' ? 4.5 : c - '0';
-  }
+       
+        return (leftSum - rightSum) != 9 * (rightQ - leftQ) / 2;
+    }
 };
